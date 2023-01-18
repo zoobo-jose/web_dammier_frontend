96ba5e05f0dcb1c9448e4fb14d10c4b133d14e95
eric <ericjosezoobo@gmail.com>
2023-01-18 22:18:26 +0100

    [update]:accueil

--- compile/tmp/middle/page/accueil.js
+++ compile/tmp/middle/page/accueil.js
@@ -1,10 +1,18 @@
 attribut.accueil={
     name:'accueil',
+    navBarIsCollapsed:false,
     /* methode d'initialisation*/
     init:function(param){
         console.log(" demarrage de accueil");
     },
     collapseNavBar(){
+        attribut.accueil.navBarIsCollapsed=!attribut.accueil.navBarIsCollapsed;
         animHtmlService.toggleClassFromClass('navbar','hidden');
+    },
+    hiddeNavBar(){
+        if(attribut.accueil.navBarIsCollapsed){
+            attribut.accueil.navBarIsCollapsed=!attribut.accueil.navBarIsCollapsed;
+            animHtmlService.toggleClassFromClass('navbar','hidden');
+        }
     }
 }
--- page/accueil/accueil.js
+++ page/accueil/accueil.js
@@ -1,10 +1,18 @@
 attribut.accueil={
     name:'accueil',
+    navBarIsCollapsed:false,
     /* methode d'initialisation*/
     init:function(param){
         console.log(" demarrage de accueil");
     },
     collapseNavBar(){
+        this_.navBarIsCollapsed=!this_.navBarIsCollapsed;
         animHtmlService.toggleClassFromClass('navbar','hidden');
+    },
+    hiddeNavBar(){
+        if(this_.navBarIsCollapsed){
+            this_.navBarIsCollapsed=!this_.navBarIsCollapsed;
+            animHtmlService.toggleClassFromClass('navbar','hidden');
+        }
     }
 }
