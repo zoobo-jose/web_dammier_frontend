9c43184832330b7b6d55b76e5909df53408ef2e1
eric <ericjosezoobo@gmail.com>
2023-01-21 20:00:10 +0100

    [feat]:compte

--- compile/tmp/middle/page/accueil.js
+++ compile/tmp/middle/page/accueil.js
@@ -68,9 +68,9 @@ attribut.accueil._child2={
         App.refresh();
     }
 }
-//_a_b_cSat Jan 21 2023 09:32:26 GMT+0100 (West Africa Standard Time)
+//_a_b_cSat Jan 21 2023 19:59:49 GMT+0100 (West Africa Standard Time)
 attribut.accueil.showLoadingPage=attribut.accueil._child2.showed;
-//_a_b_cSat Jan 21 2023 09:32:26 GMT+0100 (West Africa Standard Time)
-//_a_b_cSat Jan 21 2023 09:32:26 GMT+0100 (West Africa Standard Time)
+//_a_b_cSat Jan 21 2023 19:59:49 GMT+0100 (West Africa Standard Time)
+//_a_b_cSat Jan 21 2023 19:59:49 GMT+0100 (West Africa Standard Time)
 attribut.accueil.hiddenLoadingPage=attribut.accueil._child2.hidden;
-//_a_b_cSat Jan 21 2023 09:32:26 GMT+0100 (West Africa Standard Time)
+//_a_b_cSat Jan 21 2023 19:59:49 GMT+0100 (West Africa Standard Time)
